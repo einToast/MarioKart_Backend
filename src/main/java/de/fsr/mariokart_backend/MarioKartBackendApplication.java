ca8c53836675c72f0017ffa0a92b7427874a06df
einToast <laurin2102@gmail.com>
2024-06-05 00:46:02 +0200

    Initial commit

---
+++ src/main/java/de/fsr/mariokart_backend/MarioKartBackendApplication.java
@@ -0,0 +1,13 @@
+package de.fsr.mariokart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarioKartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarioKartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fsr/mariokart_backend/MarioKartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fsr.mariokart_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarioKartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
