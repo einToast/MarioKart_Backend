36446a083939d9f3dd971060aea906a245de60f2
einToast <laurin2102@gmail.com>
2024-12-16 16:57:30 +0100

    Update Settings - full reset -cannot open registratton if match plan is created

--- src/main/java/de/fsr/mariokart_backend/settings/service/SettingsService.java
+++ src/main/java/de/fsr/mariokart_backend/settings/service/SettingsService.java
@@ -1,8 +1,16 @@
 package de.fsr.mariokart_backend.settings.service;
 
+import de.fsr.mariokart_backend.exception.RoundsAlreadyExistsException;
+import de.fsr.mariokart_backend.match_plan.repository.GameRepository;
+import de.fsr.mariokart_backend.match_plan.repository.PointsRepository;
+import de.fsr.mariokart_backend.match_plan.repository.RoundRepository;
+import de.fsr.mariokart_backend.registration.repository.TeamRepository;
 import de.fsr.mariokart_backend.settings.model.Tournament;
 import de.fsr.mariokart_backend.settings.model.dto.TournamentDTO;
+import de.fsr.mariokart_backend.match_plan.repository.BreakRepository;
 import de.fsr.mariokart_backend.settings.repository.TournamentRepository;
+import de.fsr.mariokart_backend.survey.repository.AnswerRepository;
+import de.fsr.mariokart_backend.survey.repository.QuestionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +19,14 @@ import org.springframework.stereotype.Service;
 public class SettingsService {
 
     private final TournamentRepository tournamentRepository;
+    private final BreakRepository breakRepository;
+    private final RoundRepository roundRepository;
+    private final GameRepository gameRepository;
+    private final PointsRepository pointsRepository;
+    private final TeamRepository teamRepository;
+    private final QuestionRepository questionRepository;
+    private final AnswerRepository answerRepository;
+
 
     public TournamentDTO getSettings() {
         if (tournamentRepository.findAll().isEmpty()) {
@@ -26,23 +42,38 @@ public class SettingsService {
         return new TournamentDTO(tournamentRepository.save(new Tournament()));
     }
 
-    public TournamentDTO updateSettings(TournamentDTO tournamentDTO) {
+    public TournamentDTO updateSettings(TournamentDTO tournamentDTO) throws RoundsAlreadyExistsException {
         Tournament tournament = tournamentRepository.findAll().get(0);
         if (tournament == null) {
             throw new IllegalStateException("Settings do not exist.");
         }
+        if (tournamentDTO.getRegistrationOpen() != null && tournamentDTO.getRegistrationOpen() && !roundRepository.findAll().isEmpty()) {
+            throw new RoundsAlreadyExistsException("Matches already exist. Can't open registration.");
+        }
+
         if (tournamentDTO.getTournamentOpen() != null){
             tournament.setTournamentOpen(tournamentDTO.getTournamentOpen());
         }
         if (tournamentDTO.getRegistrationOpen() != null){
             tournament.setRegistrationOpen(tournamentDTO.getRegistrationOpen());
         }
-
         if (tournamentDTO.getMaxGamesCount() != null){
             tournament.setMaxGamesCount(tournamentDTO.getMaxGamesCount());
         }
 
         return new TournamentDTO(tournamentRepository.save(tournament));
+    }
+
+    public void reset() {
+        roundRepository.deleteAll();
+        gameRepository.deleteAll();
+        pointsRepository.deleteAll();
+        teamRepository.deleteAll();
+        questionRepository.deleteAll();
+        answerRepository.deleteAll();
+        breakRepository.deleteAll();
+        tournamentRepository.deleteAll();
+        tournamentRepository.save(new Tournament());
     }
 
 }
