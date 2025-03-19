c29e356266bc727642ed55e54670ee48aebe2152
einToast <laurin2102@gmail.com>
2025-03-19 16:54:38 +0800

    refactor(user): only use public API
    - rename Controller to PublicUserAuthController
    - delete unused admin function api

---
+++ src/main/java/de/fsr/mariokart_backend/user/controller/pub/PublicUserAuthController.java
@@ -0,0 +1,33 @@
+package de.fsr.mariokart_backend.user.controller.pub;
+
+import de.fsr.mariokart_backend.controller.annotation.ApiController;
+import de.fsr.mariokart_backend.controller.annotation.ApiType;
+import de.fsr.mariokart_backend.controller.annotation.ControllerType;
+import de.fsr.mariokart_backend.user.model.dto.AuthenticationRequestDTO;
+import de.fsr.mariokart_backend.user.model.dto.AuthenticationResponseDTO;
+import de.fsr.mariokart_backend.user.service.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@ApiController(apiType = ApiType.PUBLIC, controllerType = ControllerType.USER)
+public class PublicUserAuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponseDTO> login(@RequestBody @Valid AuthenticationRequestDTO request) {
+        try {
+            return ResponseEntity.ok().body(authenticationService.authenticateUser(request));
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+}
