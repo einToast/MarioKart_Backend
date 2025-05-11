702b45904b7236d1d04eb0503c6c3cdf776f8b3c
einToast <laurin2102@gmail.com>
2025-05-12 01:45:11 +0800

    refactor(repository): remove unused query methods from TeamRepository

--- src/main/java/de/fsr/mariokart_backend/registration/repository/TeamRepository.java
+++ src/main/java/de/fsr/mariokart_backend/registration/repository/TeamRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import de.fsr.mariokart_backend.registration.model.Team;
@@ -16,15 +15,6 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
 
     // boolean existsByCharacterName(String characterName);
     List<Team> findByFinalReadyTrue();
-
-    @Query("SELECT t FROM Team t JOIN t.points p GROUP BY t.id ORDER BY SUM(p.groupPoints) DESC")
-    List<Team> findAllByOrderByGroupPointsDesc();
-
-    @Query("SELECT t FROM Team t JOIN t.points p GROUP BY t.id ORDER BY SUM(p.finalPoints) DESC")
-    List<Team> findAllByOrderByFinalPointsDesc();
-
-    @Query("SELECT t FROM Team t JOIN t.points p GROUP BY t.id ORDER BY SUM(p.finalPoints) DESC, SUM(p.groupPoints) DESC")
-    List<Team> findAllByOrderByFinalPointsDescGroupPointsDesc();
 
     Optional<Team> findByTeamName(String teamName);
 
