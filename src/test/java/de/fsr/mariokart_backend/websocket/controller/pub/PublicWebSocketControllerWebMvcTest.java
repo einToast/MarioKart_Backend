aeda9315025eea4d89b256187e8a83674680cfd5
einToast <laurin2102@gmail.com>
2026-02-23 22:15:51 +0100

    test: add web MVC tests for PublicWebSocketController

---
+++ src/test/java/de/fsr/mariokart_backend/websocket/controller/pub/PublicWebSocketControllerWebMvcTest.java
@@ -0,0 +1,33 @@
+package de.fsr.mariokart_backend.websocket.controller.pub;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import de.fsr.mariokart_backend.testsupport.AbstractWebMvcSliceTest;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+@WebMvcTest(PublicWebSocketController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
+@Tag("web")
+class PublicWebSocketControllerWebMvcTest extends AbstractWebMvcSliceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Test
+    void contextLoads() {
+        assertThat(mockMvc).isNotNull();
+    }
+}
