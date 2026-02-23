9b865d3a2bb2ac5fd57217ecab42e7ab2d0278c4
einToast <laurin2102@gmail.com>
2026-02-23 22:14:37 +0100

    test: add web MVC tests for PublicUserAuthController

---
+++ src/test/java/de/fsr/mariokart_backend/user/controller/pub/PublicUserAuthControllerWebMvcTest.java
@@ -0,0 +1,41 @@
+package de.fsr.mariokart_backend.user.controller.pub;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import de.fsr.mariokart_backend.testsupport.AbstractWebMvcSliceTest;
+import de.fsr.mariokart_backend.user.service.AuthenticationService;
+import de.fsr.mariokart_backend.user.UserProperties;
+import de.fsr.mariokart_backend.config.CookieProperties;
+
+@WebMvcTest(PublicUserAuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
+@Tag("web")
+class PublicUserAuthControllerWebMvcTest extends AbstractWebMvcSliceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AuthenticationService authenticationService;
+
+    @MockitoBean
+    private UserProperties userProperties;
+
+    @MockitoBean
+    private CookieProperties cookieProperties;
+
+    @Test
+    void contextLoads() {
+        assertThat(mockMvc).isNotNull();
+    }
+}
