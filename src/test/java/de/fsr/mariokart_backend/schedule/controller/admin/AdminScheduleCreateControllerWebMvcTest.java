ab6843c3b46b607d68f4d8c26c8acd808ef678d0
einToast <laurin2102@gmail.com>
2026-02-23 22:09:47 +0100

    test: add web MVC tests for schedule controllers

---
+++ src/test/java/de/fsr/mariokart_backend/schedule/controller/admin/AdminScheduleCreateControllerWebMvcTest.java
@@ -0,0 +1,33 @@
+package de.fsr.mariokart_backend.schedule.controller.admin;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import de.fsr.mariokart_backend.testsupport.AbstractWebMvcSliceTest;
+import de.fsr.mariokart_backend.schedule.service.admin.AdminScheduleCreateService;
+
+@WebMvcTest(AdminScheduleCreateController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
+@Tag("web")
+class AdminScheduleCreateControllerWebMvcTest extends AbstractWebMvcSliceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AdminScheduleCreateService adminScheduleCreateService;
+
+    @Test
+    void contextLoads() {
+        assertThat(mockMvc).isNotNull();
+    }
+}
