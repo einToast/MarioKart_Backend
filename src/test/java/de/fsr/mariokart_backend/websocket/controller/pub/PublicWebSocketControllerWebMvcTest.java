84468c7d57f878b858f7ea7fb2c49d6c10f3cc87
einToast <laurin2102@gmail.com>
2026-02-24 02:15:54 +0100

    test: add unit tests for PublicWebSocketController

--- src/test/java/de/fsr/mariokart_backend/websocket/controller/pub/PublicWebSocketControllerWebMvcTest.java
+++ src/test/java/de/fsr/mariokart_backend/websocket/controller/pub/PublicWebSocketControllerWebMvcTest.java
@@ -1,18 +1,20 @@
 package de.fsr.mariokart_backend.websocket.controller.pub;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import de.fsr.mariokart_backend.testsupport.AbstractWebMvcSliceTest;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @WebMvcTest(PublicWebSocketController.class)
 @AutoConfigureMockMvc(addFilters = false)
@@ -29,5 +31,11 @@ class PublicWebSocketControllerWebMvcTest extends AbstractWebMvcSliceTest {
     @Test
     void contextLoads() {
         assertThat(mockMvc).isNotNull();
+    }
+
+    @Test
+    void httpGetIsNotMappedForWebSocketController() throws Exception {
+        mockMvc.perform(get("/public/websocket"))
+                .andExpect(status().isNotFound());
     }
 }
